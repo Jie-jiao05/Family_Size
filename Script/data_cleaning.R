d8c807d4163e8ee2419a65e793f8c02e1adedc4e
Jie-jiao05 <jiejiao404@gmail.com>
2025-02-01 18:57:32 -0500

    dd

--- Script/data_cleaning.R
+++ Script/data_cleaning.R
@@ -7,19 +7,10 @@ data <- read.csv("~/Family_Size/Data/portugal.csv")
 # Convert 'literacy' to binary (1 for 'yes', 0 for 'no')
 data$literacy <- ifelse(data$literacy == "yes", 1, 0)
 
-# Convert 'ageMarried' into numerical format using midpoints
-convert_age_married <- function(x) {
-  if (x == "30toInf") {
-    return(32.5)  # Approximate midpoint assuming 30 to ~35
-  } else if (x == "0to15") {
-    return(7.5)  # Midpoint assumption
-  } else {
-    ranges <- as.numeric(unlist(strsplit(x, "to")))
-    return(mean(ranges))
-  }
-}
-
-data$ageMarried <- sapply(data$ageMarried, convert_age_married)
+# Convert 'ageMarried' into categorical dummy variables, using '22to25' as the reference group
+data <- data %>%
+  mutate(ageMarried = as.factor(ageMarried)) %>%
+  mutate(ageMarried = relevel(ageMarried, ref = "22to25"))  # Set '22to25' as reference group
 
 # Convert 'region' into dummy variables
 data <- data %>%
@@ -37,3 +28,6 @@ summary(data)
 
 # Save cleaned dataset
 write.csv(data, "~/Family_Size/Data/cleaned_portugal.csv", row.names = FALSE)
+
+
+
